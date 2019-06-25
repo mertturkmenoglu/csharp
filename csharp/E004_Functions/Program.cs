b8ac92d76d2d3772dc9c0c085933f980174cc3f0
Mert <mertturkmenoglu99@gmail.com>
2019-06-25 23:24:36 +0300

    Add functions example

---
+++ csharp/E004_Functions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace E004_Functions
+{
+    internal static class Program
+    {
+        private static void MulValue(int value)
+        {
+            value *= 2;
+            Console.WriteLine("Inside function: " + value);
+        }
+        
+        // It is similar to C++ references
+        private static void MulRef(ref int value)
+        {
+            value *= 2;
+            Console.WriteLine("Inside function: " + value);
+        }
+        
+        public static void Main()
+        {
+            var val = 5;
+            MulValue(val);
+            Console.WriteLine("Inside main: " + val);
+            
+            // Here we must use ref keyword
+            MulRef(ref val);
+            Console.WriteLine("Inside main: " + val);
+        }
+    }
+}
