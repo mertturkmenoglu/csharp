c0b1a0ae14a67ddcc3b71eebee0abdc74461dc31
Mert <mertturkmenoglu99@gmail.com>
2019-06-25 23:13:10 +0300

    Add user input example

---
+++ csharp/E003_UserInput/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace E003_UserInput
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter your name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Please enter your age: ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Name: " + name + "\tAge: " + age);
+
+            Console.WriteLine(age >= 18 ? "You are an adult" : "You are not an adult");
+        }
+    }
+}
