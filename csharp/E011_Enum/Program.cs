8f8232d1319aa653396ccb9c870b997637a1f7de
Mert <mertturkmenoglu99@gmail.com>
2019-06-26 16:39:25 +0300

    Add enum example

---
+++ csharp/E011_Enum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace E011_Enum
+{
+    public enum EyeColor
+    {
+        Green,
+        Blue,
+        Brown,
+        Black,
+        Hazel
+    }
+
+    public class Person
+    {
+        public Person(string name, EyeColor eyeColor)
+        {
+            Name = name;
+            Eye = eyeColor;
+        }
+
+        public EyeColor Eye { get; set; }
+
+        public string Name { get; }
+    }
+    
+    internal static class Program
+    {
+        public static void Main()
+        {
+            Person person = new Person("Emily", EyeColor.Green);
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Eye);
+            person.Eye = EyeColor.Hazel;
+            Console.WriteLine(person.Eye);
+        }
+    }
+}
