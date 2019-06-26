608293351fab27d25cfdbf3e59c361ced32e93eb
Mert <mertturkmenoglu99@gmail.com>
2019-06-26 15:26:22 +0300

    Add params example

---
+++ csharp/E007_Params/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace E007_Params
+{
+    internal static class Program
+    {
+        private static void PrintInfos(int age, params string[] values)
+        {
+            Console.WriteLine("Age: " + age);
+            foreach (var value in values)
+            {
+                Console.WriteLine("Value: " + value);
+            }
+        }
+        
+        public static void Main()
+        {
+            string[] strings = {"Emily", "London", "Yellow"};
+            PrintInfos(35, strings);
+            PrintInfos(35, "Emily", "London", "Hello");
+        }
+    }
+}
