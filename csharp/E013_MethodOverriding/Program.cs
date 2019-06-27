2a303a8677cee507468e8e630555132b9445ba54
Mert <mertturkmenoglu99@gmail.com>
2019-06-27 12:29:45 +0300

    Add method overriding example

---
+++ csharp/E013_MethodOverriding/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace E013_MethodOverriding
+{
+    public class Shape
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing shape");
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            base.Draw();
+            Console.WriteLine("Drawing Rectangle");
+        }
+    }
+    
+    internal static class Program
+    {
+        public static void Main()
+        {
+            Shape s1 = new Shape();
+            Shape s2 = new Rectangle();
+            Rectangle r1 = new Rectangle();
+            
+            s1.Draw();
+            s2.Draw();
+            r1.Draw();
+        }
+    }
+}
