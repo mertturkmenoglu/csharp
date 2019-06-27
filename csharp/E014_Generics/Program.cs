6fe277e0f2f00b939ed8f59249ed432a3cc99d3e
Mert <mertturkmenoglu99@gmail.com>
2019-06-27 12:35:43 +0300

    Add generics example

---
+++ csharp/E014_Generics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace E014_Generics
+{
+    public class Container<T>
+    {
+        public Container(T value)
+        {
+            Value = value;
+        }
+        
+        public T Value { get; set; }
+    }
+
+    public static class Show
+    {
+        public static void PrintMessage<T>(T message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    
+    internal static class Program
+    {
+        public static void Main()
+        {
+            Container<int> myContainer = new Container<int>(10);
+            Console.WriteLine(myContainer.Value);
+            
+            Show.PrintMessage("Message");
+            Show.PrintMessage(myContainer.Value);
+        }
+    }
+}
